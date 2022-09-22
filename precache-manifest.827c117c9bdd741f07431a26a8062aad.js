49e48851d9dfa3cb8473b63da694c3a85374ec7d
SanDiego2049 <alabaoreoluwa@gmail.com>
2022-09-22 05:38:13 +0100

    Updates

---
+++ precache-manifest.827c117c9bdd741f07431a26a8062aad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9c15f2a240ab86ec1d2",
+    "url": "/super-robofriends/static/css/main.92339457.chunk.css"
+  },
+  {
+    "revision": "e9c15f2a240ab86ec1d2",
+    "url": "/super-robofriends/static/js/main.e9c15f2a.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/super-robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "6bd018af5101e5bd04a2",
+    "url": "/super-robofriends/static/css/2.159e51b7.chunk.css"
+  },
+  {
+    "revision": "6bd018af5101e5bd04a2",
+    "url": "/super-robofriends/static/js/2.6bd018af.chunk.js"
+  },
+  {
+    "revision": "a719e0f857034d076049",
+    "url": "/super-robofriends/static/js/runtime~main.a719e0f8.js"
+  },
+  {
+    "revision": "081354642f05cc027b87f2bcb5498e4a",
+    "url": "/super-robofriends/static/media/Alien Robot Italic.08135464.woff"
+  },
+  {
+    "revision": "0d89b9eaf0b4327f238aa07bf242d0ef",
+    "url": "/super-robofriends/index.html"
+  }
+];
